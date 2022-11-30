8a830d283503d080892e62f98cdf0c10ef1bcf7d
kiwi2180 <66368460+kiwi2180@users.noreply.github.com>
2022-11-30 19:16:29 +0800

    Update songsAndArtists.java

--- songsAndArtists.java
+++ songsAndArtists.java
@@ -1,7 +1,7 @@
 /*
 Fabella, Euge Kyle
 BSIT301A
-Data Structures and Algorithm
+Data Structures and Algorithms
 */
 
 import java.util.LinkedList;
@@ -10,42 +10,48 @@ public class songsAndArtists
 {
     public static void main(String[] args)
     {
-        //creating objects for linkedlist
+        //creating LinkedLists
         LinkedList songs = new LinkedList();
         LinkedList artists = new LinkedList();
         LinkedList playlist = new LinkedList();
 
-        //songs
+        //adding songs to the LinkedList 'songs'
         songs.add("Yuri on Ice");
         songs.add("History Maker");
         songs.add("You Only Live Once");
         songs.add("Re:frain");
         songs.add("Yoku");
 
-        //artists
+        //adding artists to the LinkedList 'artists'
         artists.add("Taro Umbayashi");
         artists.add("Dean Fujioka");
         artists.add("Wataru Hatano");
         artists.add("Aimer");
         artists.add("Eve");
 
-        //playlist
-        System.out.println("------------------------------------------------------------------");
+        //showing what's inside the LinkedList 'songs' and 'artists'
+        line();
         System.out.println(songs);
         System.out.println(artists);
-        System.out.println("------------------------------------------------------------------");
+        line();
 
-        //in this 'for loop', it will add the songs and artists to the linked list playlist
+        //in this 'for loop', it will add the contents of 'songs' and 'artists' to the LinkedList 'playlist'
         for (int i = 0; i < songs.size(); i++)
         {
             playlist.add(songs.get(i) + " - " + artists.get(i));
         }
 
-        //and lastly, it will print out its contents
+        //and lastly, it will print out its contents (playlist)
         for (int i = 0; i < playlist.size(); i++)
         {
             System.out.println(playlist.get(i));
         }
+        line();
+    }
+
+    //separator
+    public static void line()
+    {
         System.out.println("------------------------------------------------------------------");
     }
 }
